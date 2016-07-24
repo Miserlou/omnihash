ee018b6dc70be1c22a990ce6abc3d67e8b91f177
Rich Jones <rich@anomos.info>
2016-07-24 15:31:04 -0700

    0.2.0 - adds sha3 and blake

--- omnihash/omnihash.py
+++ omnihash/omnihash.py
@@ -4,6 +4,9 @@
 import click
 import hashlib
 import os
+import sha3
+
+from pyblake2 import blake2b, blake2s
 
 @click.command()
 @click.argument('hashme')
@@ -21,6 +24,7 @@ def main(hashme):
         click.echo("Hashing string '%s'.." % hashme)
         hashme_data = hashme
 
+    # Default Algos
     done = []
     for algo in sorted(hashlib.algorithms_available):
 
@@ -30,8 +34,37 @@ def main(hashme):
             
         h = hashlib.new(algo)
         h.update(hashme_data)
-        click.echo('%-*s%s' % (32, click.style(algo, fg='green') + ':', h.hexdigest()))
+        echo(algo, h.hexdigest())
         done.append(algo)
+
+    # SHA3 Family
+    s = sha3.SHA3224()
+    s.update(hashme_data)
+    echo('SHA3_224', s.hexdigest())
+
+    s = sha3.SHA3256()
+    s.update(hashme_data)
+    echo('SHA3_256', s.hexdigest())
+
+    s = sha3.SHA3384()
+    s.update(hashme_data)
+    echo('SHA3_384', s.hexdigest())
+
+    s = sha3.SHA3512()
+    s.update(hashme_data)
+    echo('SHA3_512', s.hexdigest())
+
+    # BLAKE
+    b = blake2s()
+    b.update(hashme_data.encode('utf-8'))
+    echo('BLAKE2s', b.hexdigest())
+    
+    b = blake2b()
+    b.update(hashme_data.encode('utf-8'))
+    echo('BLAKE2b', b.hexdigest())
+
+def echo(algo, digest):
+    click.echo('%-*s%s' % (32, click.style(algo, fg='green') + ':', digest))
 
 if __name__ == '__main__':
     main()
