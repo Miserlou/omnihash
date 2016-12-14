1b9c4f4f4d9004cf0327b8b58e2b1a6a5b26bf71
Kostis Anagnostopoulos <ankostis@gmail.com>
2016-12-14 13:09:07 +0100

    feat(coords): add project's coords as package special attributs

--- omnihash/__init__.py
+++ omnihash/__init__.py
@@ -1,1 +1,5 @@
 __version__ = '0.11.0'
+__license__   = "MIT License"
+__title__     = "omnihash"
+__summary__ = "Hash files/strings/streams/network-resources simultaneously in various algorithms."
+__uri__ = "https://github.com/Miserlou/omnihash"
