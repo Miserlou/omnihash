ed869adfd36d677f91a912e9ec185d9458021915
Rich Jones <rich@anomos.info>
2016-12-15 20:07:11 -0500

    0.12.0 - adds git to included hash

--- omnihash/__init__.py
+++ omnihash/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.11.0'
+__version__ = '0.12.0'
 __license__   = "MIT License"
 __title__     = "omnihash"
 __summary__ = "Hash files/strings/streams/network-resources simultaneously in various algorithms."
